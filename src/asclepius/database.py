2e0b85e9a8558adcc345540ae0051d8a85989ff9
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2022-12-11 01:40:13 +0530

    data stuff

--- src/asclepius/database.py
+++ src/asclepius/database.py
@@ -2,10 +2,47 @@ import sqlite3
 
 
 class Database:
+    """Database class for Asclepius."""
+
     def __init__(self) -> None:
-        self.connection = sqlite3.connect("database.db")
-        self.cursor = self.connection.cursor()
+        try:
+            self.connection = sqlite3.connect("src/data/asclepius.db")
+            self.cursor = self.connection.cursor()
+            print("Database connected successfully")
+        except sqlite3.OperationalError as e:
+            print("Database not found, Error: ", e)
+            print("check if the database exists in the data folder")
+
+    def add_medicine(
+        self, Mid: int, name: str, description: str, dosage: int, availability: str
+    ) -> None:
+        """Add a medicine to the database.
+
+        Args:
+            Mid (int): Medicine ID
+            name (str): Name of the medicine
+            description (str): Short description of the medicine
+            dosage (int): recommended dosage of the medicine
+            availability (bool): Is the medicine available?
+
+        Returns:
+            None
+        """
+
         self.cursor.execute(
-            "CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)"
+            "INSERT INTO medicines VALUES (?, ?, ?, ?, ?)",
+            (Mid, name, description, dosage, availability),
         )
         self.connection.commit()
+
+    def get_medicine(self, Mid: int) -> tuple:
+        """Get a medicine from the database.
+
+        Args:
+            Mid (int): Medicine ID
+
+        Returns:
+            tuple: Medicine details
+        """
+        self.cursor.execute("SELECT * FROM medicines WHERE Mid = ?", (Mid,))
+        return self.cursor.fetchone()
