11a9bb68b8f43b782041fc6a42ba6f31a0fa4d41
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2022-12-11 01:39:58 +0530

    work in progress dashboard

--- src/asclepius/dashboard.py
+++ src/asclepius/dashboard.py
@@ -1,3 +1,68 @@
+import customtkinter as ctk
+from PIL import Image
+
+
 class Dashboard:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, width: int, height: int) -> None:
+        self.root = ctk.CTk()
+        self.root.title("Asclepius")
+        self.root.resizable(False, False)
+        self.height = height
+        self.width = width
+
+        self.op_font = ctk.CTkFont(
+            family="Microsoft Sans Serif", size=30, weight="bold"
+        )
+        self.title_font = ctk.CTkFont(
+            family="Microsoft Sans Serifs", size=60, weight="bold"
+        )
+
+        self.bg = ctk.CTkImage(
+            dark_image=Image.open("assets/images/dashboard_bg.jpg"),
+            size=(self.width, self.height),
+        )
+
+    def center_window(self) -> None:
+        """Center the window."""
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        x_coord = (screen_width / 2) - (self.width / 2)
+        y_coord = (screen_height / 2) - (self.height / 2)
+        self.root.geometry(f"{self.width}x{self.height}+{int(x_coord)}+{int(y_coord)}")
+
+    def options_frame(self) -> None:
+        """Create the options frame."""
+        self.options_frame = ctk.CTkFrame(
+            self.root, width=200, height=self.height, corner_radius=5
+        )
+
+        self.options_frame.pack(side="left", fill="y", padx=(10, 0), pady=10)
+        options_title = ctk.CTkLabel(
+            self.options_frame, text="Options", font=self.op_font, corner_radius=10
+        )
+
+        options_title.grid(row=0, column=0, padx=20, pady=20)
+
+    def title_frame(self, title):
+        self.title_frame = ctk.CTkFrame(
+            self.root, width=self.width - 200, height=50, corner_radius=10
+        )
+        self.title_frame.pack(side="top", fill="x", padx=10, pady=10)
+        title_label = ctk.CTkLabel(self.title_frame, text=title, font=self.title_font)
+        title_label.grid(row=0, column=0, padx=20, pady=20)
+
+    def main_frame(self):
+        self.main_frame = ctk.CTkFrame(
+            self.root, width=self.width - 200, height=self.height - 50, corner_radius=10
+        )
+        self.main_frame.pack(side="bottom", fill="both", padx=10, pady=10)
+
+        background_label = ctk.CTkLabel(self.main_frame, image=self.bg, text="")
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    def show_dashboard(self) -> None:
+        self.options_frame()
+        self.title_frame("Dashboard")
+        self.main_frame()
+        self.center_window()
+        self.root.mainloop()
