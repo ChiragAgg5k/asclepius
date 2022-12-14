aa6eec6cb38d74f9eea4f0210fbd5a75a81f2a0b
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2022-12-14 16:17:08 +0530

    updated dashboard

--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
 from asclepius import dashboard, database, login_screen
 
-# login = login_screen.Login()
-# login.display()
+database = database.Database()
+med_data = database.get_medicines()
 
-dashboard = dashboard.Dashboard(1280, 720, "dark", "green")
+# login = login_screen.Login(
+#     appearance_mode="light", color_theme="green", width=450, height=350
+# )
+
+# while True:
+#     login.display()
+#     if login.submit:
+#         password = login.password.get()
+#         username = login.username.get()
+
+#         break
+
+# print("Username: ", username)
+# print("Password:", password)
+
+dashboard = dashboard.Dashboard(
+    width=1280, height=720, appearance="dark", theme_color="green", data=med_data
+)
 dashboard.show_dashboard()
